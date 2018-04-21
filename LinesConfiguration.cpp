7a61250690a283ca66973b1134576c241d030fc8
akrammousa <akrammousa2014@gmail.com>
2018-04-21 19:38:41 +0200

    configuration.

--- LinesConfiguration.cpp
+++ LinesConfiguration.cpp
@@ -3,6 +3,7 @@
 //
 #include <iostream>
 #include "Line.h"
+#include "Operations.h"
 using namespace std;
 #include "LinesConfiguration.h"
 void LinesConfiguration::configureLines(vector<vector> lines) {
@@ -12,14 +13,34 @@ void LinesConfiguration::configureLines(vector<vector> lines) {
 }
 void LinesConfiguration::checkLine(vector<string> line) {
     Line tempLine;
+    Operations operations;
+
     if (line[line.size()-1].find_first_of('*')!= -1){
         tempLine.setComment(line[line.size()-1]);
         if (line.size()-1 == 1){
-
-            tempLine.setOpCode(line[0]);
+            if (line[0].find_first_of('.')!= -1){
+                tempLine.setComment(line[0]);
+            }
+            else
+                tempLine.setOpCode(line[0]);
         }
         else if (line.size()-1 == 2){
-            if (line.)
+            if(operations.checkOperation(line[0]) == NULL){
+                tempLine.setLabel(line[0]);
+                if (operations.checkOperation(line[1]) == NULL)
+                    tempLine.setOpCode("NOTVALID");
+                else
+                    tempLine.setOpCode(line[1]);
+            }
+            else {
+                tempLine.setOpCode(line[0]);
+                OpGroups opGroups = operations.checkOperation(line[0]);
+                if(opGroups.checkOperand(line[1])){
+                    tempLine.setOperand(line[1]);
+                }
+                else
+                    tempLine.setOperand("NOTVALID");
+            }
         }
         else if(line.size()-1 == 3){
             tempLine.setLabel(line[0]);
