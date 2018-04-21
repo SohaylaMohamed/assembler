ee26c180eac22f8f24c9303cd29d00ceed862726
HANAN SAMIR <samirhanan32@gmail.com>
2018-04-21 19:54:16 +0200

    read file

---
+++ readFile.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <regex>
+#include "readFile.h"
+
+using namespace std;
+
+readFile::readFile(string fileName) {
+    readFile::fileName = fileName;
+}
+void readFile::read() {
+    inFile.open(fileName);
+    if (!inFile.is_open()) {
+        cout << "File Doesn't Exist!" << endl;
+        return;
+    }
+    while (inFile.good()) {
+        string thesub;
+        lineVector.clear();
+        getline(inFile, line);
+        if (Blank())
+            continue;
+        if (comment()) {
+            lineVector.push_back(line);
+            continue;
+        }
+        if(line.find_first_of('*') != -1){
+            int found=line.find_first_of('*');
+            int sizestr=line.size();
+            thesub = line.substr(found, sizestr);
+            lineVector.push_back(thesub);
+            line.erase(found, sizestr);
+        }
+        std::istringstream iss(line);
+        for(std::string line; iss >> line; )
+            lineVector.push_back(line);
+        lineVector.push_back(thesub);
+        allLines.push_back(lineVector);
+    }
+}
+
+bool readFile::comment() {
+    regex Comment("^\\s*\\..*");
+    return regex_match(line, Comment);
+}
+vector<vector<string>> readFile::getLines() {
+    return allLines;
+}
+
+    bool readFile::Blank() {
+    regex blank("\\s*");
+    return regex_match(line, blank);
+}
---
+++ readFile.h
@@ -0,0 +1,25 @@
+//
+// Created by HANAN SAMIR on 21/04/2018.
+//
+
+#ifndef ASSEMBLER_READFILE_H
+#define ASSEMBLER_READFILE_H
+using namespace std;
+class readFile {
+public:
+    readFile(string fileName);
+    void read();
+    vector<vector <string>> getLines();
+
+private:
+    ifstream inFile;
+    std::vector<std::string> lineVector;
+    vector<vector<string> > allLines;
+    string fileName = "" ,line;
+    bool comment();
+    bool Blank();
+    bool separate();
+
+
+};
+#endif //ASSEMBLER_READFILE_H
