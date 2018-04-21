87a079d7b1e9e9b7d7192085f6ddec5e22ab9f53
akrammousa <akrammousa2014@gmail.com>
2018-04-21 16:13:43 +0200

    Line.

--- Line.cpp
+++ Line.cpp
@@ -1,5 +1,6 @@
 //
 // Created by LENOVO on 21/04/2018.
 //
-
+#include <iostream>
+using namespace std;
 #include "Line.h"
--- Line.h
+++ Line.h
@@ -1,14 +1,21 @@
 //
 // Created by LENOVO on 21/04/2018.
 //
-
+#include <iostream>
+using namespace std;
 #ifndef ASSEMBLERPASS1_LINE_H
 #define ASSEMBLERPASS1_LINE_H
 
 
 class Line {
 public:
-     test;
+     int lineNo;
+     string address;
+     string label;
+     string opCode;
+     string operand;
+     string comment;
+
 };
 
 
