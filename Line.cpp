03a4908837cbb53d2e155b11d32f0ca847cb198b
akrammousa <akrammousa2014@gmail.com>
2018-04-21 16:08:20 +0200

    Line.

---
+++ Line.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LENOVO on 21/04/2018.
+//
+
+#include "Line.h"
---
+++ Line.h
@@ -0,0 +1,15 @@
+//
+// Created by LENOVO on 21/04/2018.
+//
+
+#ifndef ASSEMBLERPASS1_LINE_H
+#define ASSEMBLERPASS1_LINE_H
+
+
+class Line {
+public:
+     test;
+};
+
+
+#endif //ASSEMBLERPASS1_LINE_H
