84910b40b6e727e295284653a703ac05998b1ddd
youmna_dwidar <youmnadwidar@hotmail.com>
2018-04-27 19:48:38 +0200

    new Groups

---
+++ Operands/OpGroup10.h
@@ -0,0 +1,19 @@
+//
+// Created by HP on 4/27/2018.
+//
+
+#ifndef ASSEMBLERPASS1_OPGROUP10_H
+#define ASSEMBLERPASS1_OPGROUP10_H
+
+
+#include "OpGroups.h"
+
+class OpGroup10 : public OpGroups {
+public:
+    OpGroup10();
+    // end operator
+
+};
+
+
+#endif //ASSEMBLERPASS1_OPGROUP10_H
