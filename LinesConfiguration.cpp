533a10e7a95692d81ccefe4a30b94a5a19018682
akrammousa <akrammousa2014@gmail.com>
2018-04-27 18:01:05 +0200

    conf

--- LinesConfiguration.cpp
+++ LinesConfiguration.cpp
@@ -33,43 +33,43 @@ void LinesConfiguration::checkLine(vector<string> line) {
     }else {
         tempLine.setComment("");
     }
-        if (line.size() - 1 == 1) {
-            if (line[0].find_first_of('.') != -1) {
-                tempLine.setComment(line[0]);
-            } else {
-                tempLine.setOpCode(line[0]);
-            }
-        } else if (line.size() - 1 == 2) {
-            if (!operations.checkOperation(line[0])) {
-                tempLine.setLabel(line[0]);
-                if (!operations.checkOperation(line[1]))
-                    tempLine.setOpCode("INVALID");
-                else
-                    tempLine.setOpCode(line[1]);
-            } else {
-                tempLine.setOpCode(line[0]);
-                OpGroups *opGroups = operations.checkOperation(line[0]);
-                if ((*opGroups).checkOperand(line[1])) {
-                    tempLine.setOperand(line[1]);
-                } else
-                    tempLine.setOperand("INVALID");
-            }
-        } else if (line.size() - 1 == 3) {
-            tempLine.setLabel(line[0]);
-            if (!operations.checkOperation(line[1])) {
-                tempLine.setOpCode("INVALID");
-                tempLine.setOperand(line[2]);
-
-            } else {
-                tempLine.setOpCode(line[1]);
-                OpGroups *opGroups = operations.checkOperation(line[1]);
-                if ((*opGroups).checkOperand(line[2])) {
-                    tempLine.setOperand(line[2]);
-                } else
-                    tempLine.setOperand("INVALID");
-            }
+    if (line.size() - 1 == 1) {
+        if (line[0].find_first_of('.') != -1) {
+            tempLine.setComment(line[0]);
+        } else {
+            tempLine.setOpCode(line[0]);
         }
-        configuredLines.push_back(tempLine);
+    } else if (line.size() - 1 == 2) {
+        if (!operations.checkOperation(line[0])) {
+            tempLine.setLabel(line[0]);
+            if (!operations.checkOperation(line[1]))
+                tempLine.setOpCode("INVALID");
+            else
+                tempLine.setOpCode(line[1]);
+        } else {
+            tempLine.setOpCode(line[0]);
+            OpGroups *opGroups = operations.checkOperation(line[0]);
+            if ((*opGroups).checkOperand(line[1])) {
+                tempLine.setOperand(line[1]);
+            } else
+                tempLine.setOperand("INVALID");
+        }
+    } else if (line.size() - 1 == 3) {
+        tempLine.setLabel(line[0]);
+        if (!operations.checkOperation(line[1])) {
+            tempLine.setOpCode("INVALID");
+            tempLine.setOperand(line[2]);
+
+        } else {
+            tempLine.setOpCode(line[1]);
+            OpGroups *opGroups = operations.checkOperation(line[1]);
+            if ((*opGroups).checkOperand(line[2])) {
+                tempLine.setOperand(line[2]);
+            } else
+                tempLine.setOperand("INVALID");
+        }
+    }
+    configuredLines.push_back(tempLine);
 
 
 }
