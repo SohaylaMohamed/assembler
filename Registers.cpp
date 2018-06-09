2968ce30730cc6cc939823ea8befbc37368af3fe
Sohayla Mohamed <sohaylamohammed734@gmail.com>
2018-06-09 17:59:31 +0200

    Registers class and file

---
+++ Registers.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Sohayla Mohamed on 09/06/2018.
+//
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "Registers.h"
+
+Registers::Registers() {
+    std::string line;
+    ifstream file;
+    file.open("registers.txt");
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            istringstream iss(line);
+            std::vector<std::string> result;
+            for (std::string s; iss >> s;)
+                result.push_back(s);
+            stringstream geek(result[1]);
+            int x = 0;
+            geek >> x;
+            Registers::regNum[result[0]] = x;
+        }
+    }
+}
+
+int Registers::getRegNum(string r) {
+    return Registers::regNum[r];
+}
---
+++ Registers.h
@@ -0,0 +1,24 @@
+//
+// Created by Sohayla Mohamed on 09/06/2018.
+//
+
+#ifndef ASSEMBLERPASS1_REGISTERS_H
+#define ASSEMBLERPASS1_REGISTERS_H
+
+
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Registers {
+private:
+    map<string, int> regNum;
+public:
+    Registers();
+    int getRegNum(string r);
+
+};
+
+
+#endif //ASSEMBLERPASS1_REGISTERS_H
