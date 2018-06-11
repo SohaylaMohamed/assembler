b72691d9dc0df094d3a5b6d1c685fb763ae64c98
youmna_dwidar <youmnadwidar@hotmail.com>
2018-06-11 12:18:57 +0200

    modified

--- pass2.cpp
+++ pass2.cpp
@@ -9,31 +9,31 @@ vector<string> pass2::generateObjectCode(vector<Line> lines) {
     int format;
     SymTable symTable;
     symTable.createSymTable(lines);
-    object_code calcute(symTable);
+    object_code calculate = object_code(symTable);
     vector<string> objectCodes;
     regex regex1("^=[cCwWxX]");
     for (int i = 0; i < lines.size(); ++i) {
         format = lines[i].getFormatNo();
         switch (format) {
             case 1:
-                objectCodes.push_back(calcute.getObject_1(lines[i]));
+                objectCodes.push_back(calculate.getObject_1(lines[i]));
                 break;
             case 2:
-                objectCodes.push_back(calcute.getObject_2(lines[i]));
+                objectCodes.push_back(calculate.getObject_2(lines[i]));
                 break;
             case 3:
             case 4:
-                objectCodes.push_back(calcute.getObject_3(lines[i]));
+                objectCodes.push_back(calculate.getObject_3(lines[i]));
                 break;
             case 0 :
                 Line line = lines[i];
                 if (line.getOpCode() == "WORD" || line.getOpCode() == "BYTE") {
-                    vector<string> codes = calcute.getObject_dir(line);
+                    vector<string> codes = calculate.getObject_dir(line);
                     for (int j = 0; j < codes.size(); ++j) {
                         objectCodes.push_back(codes[j]);
                     }
                 } else if (regex_match(line.getOpCode(), regex1)) {
-                    objectCodes.push_back(calcute.getObject_lit(line));
+                    objectCodes.push_back(calculate.getObject_lit(line));
                 }
                 break;
         }
